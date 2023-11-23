24b05596b7ce3c8c87440a6180c773a0364477b7
Pratik Borkar <pratikab01@gmail.com>
2023-11-23 15:37:30 +0530

    Update Coal_Plants_Analysis.py

--- Coal_Plants_Analysis.py
+++ Coal_Plants_Analysis.py
@@ -49,7 +49,7 @@ with col2:
 # Align the button in the middle
 st.markdown("<div class='centered'> </div>", unsafe_allow_html=True)
 
-button_col = st.columns([8, 8, 3])
+button_col = st.columns([1, 8, 3])
 with button_col[1]:
     if st.button("Analyse", type="primary", help="Click to analyze", key="analyse_button"):
         if result1 == "Financial Analysis" and result2 == 'All (2012-22)':
@@ -88,3 +88,37 @@ with button_col[1]:
             )
 
             st.altair_chart(bar_chart_dispatch, use_container_width=True)
+        if result1 == "Raniganj Coalfield" and result2 == '2021-22':
+            image1 = st.image('raniganj.png', use_column_width=True)
+            st.write(
+                """
+                **Coal Mining Plant Name:** Raniganj Coalfield\n
+                **Origin:** 1774\n
+                **Open-pit/Underground:** Open-pit\n
+                **Produce:** Bituminous coal/non-coking coal\n
+                **GCV:** Moderate\n
+                **Production Rate/day:** 136k tonnes/day\n
+                **Production Rate/week:** 952,000 tonnes/week\n
+                **Production Rate/month:** 4,080,000 tonnes/month\n
+                **Production Rate/year:** 49,640,000 tonnes/year\n
+                **Revenue (in million INR):** 248200 million\n
+                **Maintenance Cost:** 58550 million INR/year\n
+                **Area:** 443.50 km^2\n
+                **Coal Reserves:** 49.17 billion tonnes\n
+                **Annual Turnover:** ₹12,076.17 crore (US$1.5 billion)\n
+                **Profit before:** ₹1,897.18 crore (US$240 million)\n
+                **Operating Mines:** 98\n
+                **Underground Mines:** 77\n
+                **Open Pit:** 21\n
+                **Major Problem:** Coal fire\n
+                **Mined By:** 90% [ Eastern Coalfield Limited (ECL)] a subsidiary of Coal India Limited (CIL) and a small area is mined by Bharat Coking Coal Limited (BCCL)\n
+                **Grade Type:** G3\n
+                **Size:** 250\n
+                **Fixed Carbon:** 78\n
+                **Ash Content:** 8\n
+                **Depth:** 325\n
+                **Deposits:** Specially for export market\n
+                **Government/Private:** Private
+                """
+            )
+    
